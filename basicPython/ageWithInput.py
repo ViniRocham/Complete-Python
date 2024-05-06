15b7f56512b8545d3e96d7c667984c66c8718dcb
ViniRocham <viniciusrocha1004@gmail.com>
2024-05-06 15:38:40 -0300

    settings

--- basicPython/ageWithInput.py
+++ basicPython/ageWithInput.py
@@ -1,7 +1,7 @@
-year_age = input("What year were you born? ")
-print(type(year_age))
+year_of_birth = input("What year were you born? ")
+print(type(year_of_birth))
 
-age = 2024 - int(year_age)
+age = 2024 - int(year_of_birth)
 print(type(age))
 
 print(age)
