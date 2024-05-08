fec462c3531a5e35d0f3c0a3cb8f28488f894db0
ViniRocham <viniciusrocha1004@gmail.com>
2024-05-08 17:03:28 -0300

    math operations

---
+++ operators/MathOparations.py
@@ -0,0 +1,14 @@
+#parentheses, exponential, multiplication, division, sum, subibration
+
+calculation1 = 2 + 2 * 3 /2
+print(calculation1)
+
+
+calculation2 = (2 + 2) * 3
+print(calculation2)
+
+calculation3 = 2 ** 3
+print(calculation3)
+
+
+
