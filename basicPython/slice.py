bf7199da3d8b13eb735cd8ce2770ed3c19ccb83c
ViniRocham <viniciusrocha1004@gmail.com>
2024-05-08 15:17:00 -0300

    slice

---
+++ basicPython/slice.py
@@ -0,0 +1,23 @@
+
+fruit = 'Watermeloon'
+#index   012345678910
+
+#print value order
+print(fruit[10])
+
+#print value order 0 to 4
+print(fruit[0:5])
+
+#print value in reverse
+print(fruit[-1])
+
+
+#with number
+
+value = 99.75
+value = str(value)
+#index      01234
+
+print(value[3:5])
+print(value[2:])
+print(value[-1])
