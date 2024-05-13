72b655dca88303869e380584074e4b24d898b6a9
ViniRocham <viniciusrocha1004@gmail.com>
2024-05-13 15:19:23 -0300

    elif

---
+++ fluxControl/if_else.py
@@ -0,0 +1,10 @@
+
+speed = 40
+
+if speed > 110:
+    print('above the permitted speed')
+    print('Please, slow down')
+elif speed < 60:
+    print('Please, drive above to 80')
+else:
+    print('Ok speed')
